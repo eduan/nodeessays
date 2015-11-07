57f0126e3fa140eb499e8e1f1ad700dc73fdc611
Eduan <eduanlenine@yahoo.com.br>
2015-11-06 22:54:29 -0200

    Reusing server

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
+var port = 8080;
+var output = '';
 var http = require('http');
 
-http.createServer(function(request, response){
-	response.writeHead(200,{'Content-Type' : 'text/plain'});
-	response.end("Hello, World!");
-}).listen(8081);
+exports.end = function() {
+	console.log('Initializing Server on 0.0.0.0:' + port);
+    http.createServer(function(request, response){
+		response.writeHead(200, {'Content-Type' : 'text/plain'});
+		response.write(output);
+		response.end();
+	}).listen(port);
+};
+
+exports.echo = function(stream) {
+	output += stream; 
+	return this;
+};
+
+exports.setPort = function(portParam) {
+	port = port; 
+};

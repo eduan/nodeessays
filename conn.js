7cedd5dbce35e42725e1a05c1692437015c00b09
Eduan <eduanlenine@yahoo.com.br>
2015-11-06 23:51:03 -0200

    morse digital

--- conn.js
+++ conn.js
@@ -1,2 +1,12 @@
 var server = require("./server.js")
-server.echo('Hello, World!').end();
+mysql = require("mysql"); 
+     
+var connection = mysql.createConnection({ 
+    user: "root", 
+    password: "q1w2e3", 
+    database: ""
+}); 
+
+connection.query('SELECT 1', server.echo(JSON.stringify(rows)));
+
+server.end();

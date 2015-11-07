1b72d321838924a96e1d5a789a83e44421b6139d
Eduan <eduanlenine@yahoo.com.br>
2015-11-06 22:59:14 -0200

    space

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ var output = '';
 var http = require('http');
 
 exports.end = function() {
-	console.log('Initializing Server on 0.0.0.0:' + port);
+    console.log('Initializing Server on 0.0.0.0:' + port);
     http.createServer(function(request, response){
-		response.writeHead(200, {'Content-Type' : 'text/plain'});
-		response.write(output);
-		response.end();
-	}).listen(port);
+        response.writeHead(200, {'Content-Type' : 'text/plain'});
+        response.write(output);
+        response.end();
+    }).listen(port);
 };
 
 exports.echo = function(stream) {
-	output += stream; 
-	return this;
+    output += stream; 
+    return this;
 };
 
 exports.setPort = function(portParam) {
-	port = port; 
+    port = port; 
 };
